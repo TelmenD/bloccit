b574df04378e5e06d194c35bda22d5a49729d9fc
Telmen Davaadorj <d.telmen@gmail.com>
2015-02-17 00:53:19 +0800

    Complete Seed Data assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,39 @@
 require 'faker'
  
- # Create Posts
- 50.times do
-   Post.create!(
-     title:  Faker::Lorem.sentence,
-     body:   Faker::Lorem.paragraph
-   )
- end
- posts = Post.all
- 
- # Create Comments
- 100.times do
-   Comment.create!(
+posts = Post.all
+
+unique_title = "I think this is unique title :)"
+
+unique_post_body = "Unique post body is here :)"
+
+if (posts.select {|post| post.title == unique_title}).empty?
+  if (posts.select {|post| post.body == unique_post_body}).empty?
+    Post.create!(
+      title:  unique_title,
+      body:   unique_body
+    )
+  end
+end
+
+
+unique_body = "I think this is unique comment body"
+
+unique_comment = true;
+posts.each do |post|
+  if !(post.comments.select {|comment| comment.body == unique_body}).empty?
+    unique_comment = false;
+  end
+end
+
+if unique_comment
+  Comment.create!(
      post: posts.sample,
-     body: Faker::Lorem.paragraph
-   )
- end
+     body: unique_body
+  )
+
+end
+ 
  
  puts "Seed finished"
- puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
+ puts "There are #{Post.count} posts"
+ puts "There are #{Comment.count} comments"
